4b52ad77458e64960d2e7d4619a43f3bdc137373
Ben Straub <ben.x.straub@gsk.com>
2023-02-10 21:15:35 +0000

    #26 Moving code to inst/templates.  Lock moved to 16 version

--- R/IS_SUPPIS.R
+++
@@ -1,4 +0,0 @@
-# STEP 1 - combine IS with SUPPIS.
-
-# Please, upload IS and SUPPIS from MOCK DATA folder.
-is_suppis <- combine_supp(is, suppis)
---
+++ inst/templates/ad_adis.R
@@ -0,0 +1,14 @@
+# Name: ADIS
+#
+# Label: Immunogenicity Analysis
+#
+# Input: xx, xx, xx
+library(admiral)
+library(admiral.test) # Contains example datasets from the CDISC pilot project
+
+# Add your template ADaM script code
+
+# STEP 1 - combine IS with SUPPIS.
+
+# Please, upload IS and SUPPIS from MOCK DATA folder.
+is_suppis <- combine_supp(is, suppis)
