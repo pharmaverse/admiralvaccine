39da460b3c34a8b2fee7cb3f3fec7288294a4b8f
Ben Straub <ben.x.straub@gsk.com>
2023-02-11 19:44:44 +0000

    #26 Loading data into template

--- inst/templates/ad_adis.R
+++ inst/templates/ad_adis.R
@@ -12,6 +12,9 @@ library(metatools)
 
 # STEP 1 - combine IS with SUPPIS.
 
+data("is")
+data("suppis")
+
 # Please, upload IS and SUPPIS from MOCK DATA folder.
 is_suppis <- combine_supp(is, suppis)
 
