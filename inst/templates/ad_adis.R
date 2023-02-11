515b3a54a0534e79666e946f427f9752a06d455f
Ben Straub <ben.x.straub@gsk.com>
2023-02-11 18:47:13 +0000

    merged in code from #36 and documented data objects

--- inst/templates/ad_adis.R
+++ inst/templates/ad_adis.R
@@ -14,3 +14,28 @@ library(metatools)
 
 # Please, upload IS and SUPPIS from MOCK DATA folder.
 is_suppis <- combine_supp(is, suppis)
+
+adis <- is_suppis %>%
+  mutate(
+    AVISITN = as.numeric(VISITNUM),
+    AVISIT = case_when(
+      VISITNUM == 10 ~ "Visit 1",
+      VISITNUM == 20 ~ "Visit 2",
+      VISITNUM == 30 ~ "Visit 3",
+      VISITNUM == 40 ~ "Visit 4",
+      is.na(VISITNUM) ~ NA_character_
+    ),
+    ATPTN = as.numeric(VISITNUM / 10),
+    ATPT = case_when(
+      VISITNUM == 10 ~ "Visit 1 (Day 1)",
+      VISITNUM == 20 ~ "Visit 2 (Day #)",
+      VISITNUM == 30 ~ "Visit 3 (Day #)",
+      VISITNUM == 40 ~ "Visit 4 (Day #)",
+      is.na(VISITNUM) ~ NA_character_
+    ),
+    ATPTREF = case_when(
+      VISITNUM %in% c(10, 20) ~ "FIRST TREATMENT",
+      VISITNUM %in% c(30, 40) ~ "SECOND TREATMENT",
+      is.na(VISITNUM) ~ NA_character_
+    )
+  )
