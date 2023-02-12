deebbe9c522b2b88e84df65e65e461c86dd98e33
Ben Straub <ben.x.straub@gsk.com>
2023-02-12 01:33:42 +0000

    temporary rename of vx SDTM datasets to pass R package checks

--- R/data.R
+++ R/data.R
@@ -8,8 +8,56 @@
 
 #' Supplemental IS Dataset
 #'
-#' A SDTM CM dataset
+#' A SDTM Supplemental IS dataset
 #'
 #' @keywords dataset
 #' @family dataset
 "suppis"
+
+#' Supplemental AE Dataset
+#'
+#' A SDTM AE dataset
+#'
+#' @keywords dataset
+#' @family dataset
+"ae"
+
+#' CE Dataset
+#'
+#' A SDTM CE dataset
+#'
+#' @keywords dataset
+#' @family dataset
+"ce"
+
+#' DM Dataset
+#'
+#' A SDTM DM dataset
+#'
+#' @keywords dataset
+#' @family dataset
+"dm"
+
+#' EX Dataset
+#'
+#' A SDTM EX dataset
+#'
+#' @keywords dataset
+#' @family dataset
+"ex"
+
+#' FACE Dataset
+#'
+#' A SDTM FACE dataset
+#'
+#' @keywords dataset
+#' @family dataset
+"face"
+
+#' VS Dataset
+#'
+#' A SDTM VS dataset
+#'
+#' @keywords dataset
+#' @family dataset
+"vs"
