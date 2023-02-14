44e7a40ea9b9f364618b34f1e5a18ca81c840af4
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-02-14 14:51:48 +0530

    Update derive_param_diam_to_sev.R
    Ran the lintr

--- R/derive_param_diam_to_sev.R
+++ R/derive_param_diam_to_sev.R
@@ -7,7 +7,7 @@
 #' @param dataset Input data set
 #'
 #' The variables `USUBJID`, `APTPTREF`, `FAOBJ`, `FASCAT`,`AVAL`, `AVALC`,
-#' `FAOBJ`, `FATESTCD` and `FATEST` are expected for input data set.(`data set`)
+#' `FAOBJ`, `FATESTCD` and `FATEST` are expected for Input data set.(`data set`)
 #'
 #' @param filter_faobj Event filter
 #'
@@ -22,9 +22,9 @@
 #' *Default: "DIAMETER"*
 #' *Permitted Value*: A character scalar.
 #'
-#' Helps to filter the diameter records to derive the severity records by passing
-#' the `FATESTCD` value for diameter  which is corresponding to the specified
-#' events in `filter_faobj`.
+#' Helps to filter the diameter records to derive the severity records by 
+#' passing the `FATESTCD` value for diameter  which is corresponding to the 
+#' specified events in `filter_faobj`.
 #'
 #' @param testcd_sev `FATESTCD`value for severity
 #'
@@ -34,11 +34,11 @@
 #' Assign the value for `FATESTCD` variable to indicate the severity records.
 #' Ignore the argument if you want to set the default value.
 #'
-#' *Note: This argument value will be used to check whether input data set has*
-#' *Severity records for specified `filter_faobj` event. If it has, those records*
-#' *will be removed and new severity records will be derived from diameters.*
+#' *Note*: This argument value will be used to check whether input data set has 
+#' Severity records for specified `filter_faobj` event. If it has, those records
+#' will be removed and new severity records will be derived from diameters.
 #'
-#' @param test_sev `FATEST` value foe severity
+#' @param test_sev `FATEST` value for severity
 #'
 #' *Default: "Severity"*
 #' *Permitted Value*: A Character scalar
@@ -52,8 +52,8 @@
 #' *Default: c(0,2)*
 #' *Permitted value*: A Numeric vector
 #'
-#' The `none` and the following arguments(`mild`, `mode` and `sev` ) will be used
-#'  for assigning the diameter limits to derive the `AVALC`(severity grade).
+#' The `none` and the following arguments(`mild`, `mode` and `sev` ) will be 
+#' used for assigning the diameter limits to derive the `AVALC`(severity grade).
 #'
 #'  *Note: Use the limit reference to pass the values in argument*
 #'  *Since the condition was coded like this,*
@@ -81,17 +81,18 @@
 #' *Permitted value*: A Numeric vector
 #'
 #' @details
-#' 1. Pass the input data set in `dataset` with required variables and `AVAL` should
-#' have the diameter values for the events(`FAOBJ`) specified in `filter_faobj`.
-#' 2. If the SDTM data set has severity record for redness and swelling, User can
-#'  skip this function to keep the SDTM level severity records for further
+#' 1. Pass the Input data set in `dataset` with required variables and `AVAL` 
+#' should have the diameter values for the events(`FAOBJ`) specified in 
+#' `filter_faobj`.
+#' 2. If the SDTM data set has severity record for redness and swelling, User 
+#' can skip this function to keep the SDTM level severity records for further
 #'  derivation.
 #' 3. If User want to derive the severity records from the diameter records even
 #'  that data set has severity records for Redness and swelling. This function
-#'  will remove the existing severity records and it will create the new severity
-#'  records as`AVALC` which is derived from diameter. and `AVAL` will be derived
-#'  as numeric severity grade to get the maximum severity records which will be
-#'  derived by `derive_param_maxsev.R`.
+#'  will remove the existing severity records and it will create the new 
+#'  severity records as`AVALC` which is derived from diameter. and `AVAL` will 
+#'  be derived as numeric severity grade to get the maximum severity records 
+#'  which will be derived by `derive_param_maxsev.R`.
 #' 4. Pass the values in severity grade arguments(`none`, `mild`, `mod`, `sev`)
 #'  as per the study needs.
 #' 5.User can pass the events which has diameter values to derive their severity
@@ -99,34 +100,34 @@
 #'  grade limits.
 #'
 #' @return
-#' The input data with the new severity records for Redness and swelling which is
-#' specified in `filter_faobj` and AVAL, AVALC will be derived and fatestcd, fatest
-#' will be changed as per the values
+#' The Input data with the new severity records for Redness and swelling which 
+#' is specified in `filter_faobj` and AVAL, AVALC will be derived and fatestcd,
+#'  fatest will be changed as per the values
 #' @export
 #'
 #' @examples
-#' input <- tribble(
-#'   ~USUBJID,     ~FAOBJ,            ~AVAL,  ~AVALC,  ~ATPTREF,
-#'   "XYZ1001",    "REDNESS",           7.5,   7.5,    "VACCINATION 1",
-#'   "XYZ1001",    "REDNESS",           3.5,   3.5,    "VACCINATION 1",
-#'   "XYZ1001",    "REDNESS",             2,     2,    "VACCINATION 1",
-#'   "XYZ1001",    "REDNESS",           1.8,   1.8,    "VACCINATION 1",
-#'   "XYZ1001",    "REDNESS",           1.4,   1.4,    "VACCINATION 1",
-#'   "XYZ1002",    "REDNESS",          11.1,  11.1,    "VACCINATION 2",
-#'   "XYZ1002",    "REDNESS",           7.4,   7.4,    "VACCINATION 2",
-#'   "XYZ1002",    "REDNESS",             6,     6,    "VACCINATION 2",
-#'   "XYZ1002",    "REDNESS",           2.1,   2.1,    "VACCINATION 2",
-#'   "XYZ1002",    "REDNESS",           1.1,   1.1,    "VACCINATION 2",
-#'   "XYZ1001",    "SWELLING",          5.5,   5.5,    "VACCINATION 1",
-#'   "XYZ1001",    "SWELLING",          2.5,   2.5,    "VACCINATION 1",
-#'   "XYZ1001",    "SWELLING",            2,     2,    "VACCINATION 1",
-#'   "XYZ1001",    "SWELLING",          1.8,   1.8,    "VACCINATION 1",
-#'   "XYZ1001",    "SWELLING",          1.4,   1.4,    "VACCINATION 1",
-#'   "XYZ1002",    "SWELLING",         10.1,  10.1,    "VACCINATION 2",
-#'   "XYZ1002",    "SWELLING",          7.1,   7.1,    "VACCINATION 2",
-#'   "XYZ1002",    "SWELLING",            5,     5,    "VACCINATION 2",
-#'   "XYZ1002",    "SWELLING",          1.8,   1.8,    "VACCINATION 2",
-#'   "XYZ1002",    "SWELLING",          1.4,   1.4,    "VACCINATION 2"
+#' Input <- tribble(
+#'   ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF,
+#'   "XYZ1001", "REDNESS", 7.5, 7.5, "VACCINATION 1",
+#'   "XYZ1001", "REDNESS", 3.5, 3.5, "VACCINATION 1",
+#'   "XYZ1001", "REDNESS", 2, 2, "VACCINATION 1",
+#'   "XYZ1001", "REDNESS", 1.8, 1.8, "VACCINATION 1",
+#'   "XYZ1001", "REDNESS", 1.4, 1.4, "VACCINATION 1",
+#'   "XYZ1002", "REDNESS", 11.1, 11.1, "VACCINATION 2",
+#'   "XYZ1002", "REDNESS", 7.4, 7.4, "VACCINATION 2",
+#'   "XYZ1002", "REDNESS", 6, 6, "VACCINATION 2",
+#'   "XYZ1002", "REDNESS", 2.1, 2.1, "VACCINATION 2",
+#'   "XYZ1002", "REDNESS", 1.1, 1.1, "VACCINATION 2",
+#'   "XYZ1001", "SWELLING", 5.5, 5.5, "VACCINATION 1",
+#'   "XYZ1001", "SWELLING", 2.5, 2.5, "VACCINATION 1",
+#'   "XYZ1001", "SWELLING", 2, 2, "VACCINATION 1",
+#'   "XYZ1001", "SWELLING", 1.8, 1.8, "VACCINATION 1",
+#'   "XYZ1001", "SWELLING", 1.4, 1.4, "VACCINATION 1",
+#'   "XYZ1002", "SWELLING", 10.1, 10.1, "VACCINATION 2",
+#'   "XYZ1002", "SWELLING", 7.1, 7.1, "VACCINATION 2",
+#'   "XYZ1002", "SWELLING", 5, 5, "VACCINATION 2",
+#'   "XYZ1002", "SWELLING", 1.8, 1.8, "VACCINATION 2",
+#'   "XYZ1002", "SWELLING", 1.4, 1.4, "VACCINATION 2"
 #' ) %>% mutate(
 #'   AVALC = as.character(AVALC),
 #'   FATEST = "Diameter",
@@ -134,17 +135,16 @@
 #' )
 #'
 #' output <- derive_param_diam_to_sev(
-#'   dataset = input,
+#'   dataset = Input,
 #'   filter_faobj = c("REDENSS", "SWELLING"),
 #'   filter_diam = "DIAMETER",
 #'   testcd_sev = "SEV",
 #'   test_sev = "Severity"
 #' )
 #'
-#' Creating function
 derive_param_diam_to_sev <- function(dataset = NULL,
                                      filter_diam = "DIAMETER",
-                                     filter_faobj = c('REDNESS', 'SWELLING'),
+                                     filter_faobj = c("REDNESS", "SWELLING"),
                                      testcd_sev = "SEV",
                                      test_sev = "Severity/Intensity",
                                      none = c(0, 2),
@@ -153,45 +153,48 @@ derive_param_diam_to_sev <- function(dataset = NULL,
                                      sev = 10) {
   #-------------------------------------------------------------------------------
   # assertion checks
-  #-------------------------------------------------------------------------------
+  #-----------------------------------------------------------------------------
   assert_data_frame(dataset,
-                    required_vars = vars(USUBJID, AVAL, AVALC,
-                                         FAOBJ, FATEST, FATESTCD))
-  
-  assert_numeric_vector(arg = c(none, mild, mod, sev), optional = F)
-  
+    required_vars = vars(
+      USUBJID, AVAL, AVALC,
+      FAOBJ, FATEST, FATESTCD
+    )
+  )
+
+  assert_numeric_vector(arg = c(none, mild, mod, sev), optional = FALSE)
+
   assert_character_vector(
     arg = c(filter_diam, filter_faobj, testcd_sev, test_sev),
-    optional = F
+    optional = FALSE
   )
-  
-  #-------------------------------------------------------------------------------
-  # Checking & Removing the records which has severity records for the given FAOBJ
-  #-------------------------------------------------------------------------------
+
+  #----------------------------------------------------------------------------
+  # Checking & Removing the records which has severity records for the FAOBJ
+  #-----------------------------------------------------------------------------
   diam <- dataset %>% filter(FAOBJ %in% filter_faobj)
-  
+
   if (testcd_sev %in% diam$FATESTCD) {
-    Input <-
+    fil_rec <-
       dataset %>% filter(FATESTCD != testcd_sev &
-                           !(FAOBJ %in% filter_faobj))
-    
-  } else{
-    Input <- dataset
+        !(FAOBJ %in% filter_faobj))
+  } else {
+    fil_rec <- dataset
   }
-  #-------------------------------------------------------------------------------
+  #-----------------------------------------------------------------------------
   # Replacing FATESTCD and FATEST for Diameter with Severity
-  #-------------------------------------------------------------------------------
+  #-----------------------------------------------------------------------------
   if (filter_diam %in% diam$FATESTCD) {
-    Input <- dataset
-    
+    fil_rec <- dataset
+
     sev <-
-      Input %>% filter(FATESTCD %in% filter_diam &
-                         FAOBJ %in% filter_faobj) %>%
+      fil_rec %>%
+      filter(FATESTCD %in% filter_diam &
+        FAOBJ %in% filter_faobj) %>%
       mutate(
         FATESTCD = testcd_sev,
         FATEST = test_sev,
         DTYPE = "DERIVED",
-        #Deriving AVALC
+        # Deriving AVALC
         AVALC = if_else(
           none[1] <= AVAL & AVAL <= none[2],
           "NONE",
@@ -205,7 +208,7 @@ derive_param_diam_to_sev <- function(dataset = NULL,
             )
           )
         ),
-        #Deriving AVAL
+        # Deriving AVAL
         AVAL = case_when(
           AVALC == "NONE" ~ 0,
           AVALC == "MILD" ~ 1,
@@ -214,13 +217,11 @@ derive_param_diam_to_sev <- function(dataset = NULL,
         ),
         AVAL = as.numeric(AVAL)
       )
-    #binding with input data set
-    finalsev <- bind_rows(sev, Input)
-    
+    # binding with Input data set
+    finalsev <- bind_rows(sev, fil_rec)
+
     return(data.frame(finalsev))
-    
-    
-  } else{
+  } else {
     print(paste0(filter_diam, " ", "doesn't exist in the filtered record"))
   }
 }
