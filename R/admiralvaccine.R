41c659cf06fc6eefdebd1b653312825960390c15
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-02-20 14:38:03 +0530

    Update admiralvaccine.R
    Added exprs

--- R/admiralvaccine.R
+++ R/admiralvaccine.R
@@ -13,7 +13,7 @@
 #'             is_logical is_quosure is_quosures is_symbol new_formula
 #'             parse_expr parse_exprs quo quo_get_expr quo_is_call
 #'             quo_is_missing quo_is_null quo_is_symbol quos quo_squash quo_text
-#'             set_names sym syms type_of warn quo_set_env quo_get_env
+#'             set_names sym syms type_of warn quo_set_env quo_get_env exprs
 #' @importFrom utils capture.output str
 #' @importFrom purrr map map2 map_chr map_lgl reduce walk keep map_if transpose
 #'             flatten every modify_at modify_if reduce compose
