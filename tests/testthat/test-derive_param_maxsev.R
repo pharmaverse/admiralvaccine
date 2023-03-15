943932f228f2ad48489619b05f80b55e49592161
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-03-15 17:55:39 +0530

    Update test-derive_param_maxsev.R

--- tests/testthat/test-derive_param_maxsev.R
+++ tests/testthat/test-derive_param_maxsev.R
@@ -1,68 +1,66 @@
 # test_derive_param_maxsev
-
 library(admiraldev)
 library(rlang)
-library(tidyverse)
-library(diffdf)
+library(dplyr)
 # test case 1 -------------------------------------------------------------
 
 # testthat
 testthat::test_that("derive_param_maxsev Test 1: Check whether`AVAL`is derived
                     when AVAL is NA", {
-  # input data
-  input <- tribble(
-    ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF, ~FATEST, ~FATESTCD, ~FASCAT,
-    "XYZ1001", "REDNESS", 1, "MILD", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 2, "MODERATE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 3, "SEVERE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 0, "NONE", "VACC2", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "CHILLS", NA, "MODERATE", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1001", "CHILLS", NA, "MILD", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1001", "CHILLS", NA, "NONE", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", NA, "MILD", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", NA, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", NA, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC"
-  )
+                      # input data
+                      input <- tribble(
+                        ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF, ~FATEST, ~FATESTCD, ~FASCAT,
+                        "XYZ1001", "REDNESS", 1, "MILD", "VACC1", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "REDNESS", 2, "MODERATE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "REDNESS", 3, "SEVERE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "REDNESS", 0, "NONE", "VACC2", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "CHILLS", NA, "MODERATE", "VACC1", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1001", "CHILLS", NA, "MILD", "VACC1", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1001", "CHILLS", NA, "NONE", "VACC1", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1002", "CHILLS", NA, "MILD", "VACC2", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1002", "CHILLS", NA, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1002", "CHILLS", NA, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC"
+                      )
 
 
-  format_aval <- function(x) {
-    case_when(
-      x == "NONE" ~ 0,
-      x == "MILD" ~ 1,
-      x == "MODERATE" ~ 2,
-      x == "SEVERE" ~ 3
-    )
-  }
+                      format_aval <- function(x) {
+                        case_when(
+                          x == "NONE" ~ 0,
+                          x == "MILD" ~ 1,
+                          x == "MODERATE" ~ 2,
+                          x == "SEVERE" ~ 3
+                        )
+                      }
 
-  expected1 <- input %>%
-    mutate(AVAL = format_aval(AVALC)) %>%
-    arrange(USUBJID, FATESTCD, FATEST, FASCAT, FAOBJ, ATPTREF, AVAL) %>%
-    group_by(USUBJID, FASCAT, FATESTCD, FATEST, FAOBJ, ATPTREF) %>%
-    slice_tail(n = 1) %>%
-    filter(FATESTCD == "SEV" & FAOBJ != "REDNESS") %>%
-    mutate(DTYPE = "MAXIMUM", FATESTCD = "MAXSEV", FATEST = "Maximum severity")
+                      expected1 <- input %>%
+                        mutate(AVAL = format_aval(AVALC)) %>%
+                        arrange(USUBJID, FATESTCD, FATEST, FASCAT, FAOBJ, ATPTREF, AVAL) %>%
+                        group_by(USUBJID, FASCAT, FATESTCD, FATEST, FAOBJ, ATPTREF) %>%
+                        slice_tail(n = 1) %>%
+                        filter(FATESTCD == "SEV" & FAOBJ != "REDNESS") %>%
+                        mutate(DTYPE = "MAXIMUM", FATESTCD = "MAXSEV", FATEST = "Maximum severity")
 
-  # expected dataset
-  expected <- bind_rows(input %>% mutate(AVAL = format_aval(AVALC)), expected1)
+                      # expected dataset
+                      expected <- bind_rows(input %>% mutate(AVAL = format_aval(AVALC)), expected1)
 
 
-  # actual dataset
-  actual <- derive_param_maxsev(
-    dataset = input,
-    filter_sev = "SEV",
-    exclude_events = "REDNESS",
-    test_maxsev = "Maximum severity",
-    testcd_maxsev = "MAXSEV",
-    by_vars = exprs(USUBJID, FAOBJ, ATPTREF)
-  )
-  expect_dfs_equal(actual,
-    expected,
-    keys = c(
-      "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
-      "ATPTREF", "AVALC", "AVAL"
-    )
-  )
-})
+                      # actual dataset
+                      actual <- derive_param_maxsev(
+                        dataset = input,
+                        filter_sev = "SEV",
+                        exclude_events = "REDNESS",
+                        test_maxsev = "Maximum severity",
+                        testcd_maxsev = "MAXSEV",
+                        by_vars = exprs(USUBJID, FAOBJ, ATPTREF)
+                      )
+                      expect_dfs_equal(actual,
+                                       expected,
+                                       keys = c(
+                                         "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
+                                         "ATPTREF", "AVALC", "AVAL"
+                                       )
+                      )
+                    })
 
 
 
@@ -70,50 +68,56 @@ testthat::test_that("derive_param_maxsev Test 1: Check whether`AVAL`is derived
 
 testthat::test_that("derive_param_maxsev Test 2: Check whether`AVAL`is derived
                     eventhough AVAL is populated", {
-  # input data
-  input <- tribble(
-    ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF, ~FATEST, ~FATESTCD, ~FASCAT,
-    "XYZ1001", "REDNESS", 1, "MILD", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 2, "MODERATE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 3, "SEVERE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 0, "NONE", "VACC2", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "CHILLS", 1, "MODERATE", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1001", "CHILLS", 2, "MILD", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1001", "CHILLS", 3, "NONE", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 4, "MILD", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 6, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 7, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC"
-  )
+                      # input data
+                      input <- tribble(
+                        ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF, ~FATEST, ~FATESTCD, ~FASCAT,
+                        "XYZ1001", "REDNESS", 1, "MILD", "VACC1", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "REDNESS", 2, "MODERATE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "REDNESS", 3, "SEVERE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "REDNESS", 0, "NONE", "VACC2", "Severity", "SEV", "ADMIN-SITE",
+                        "XYZ1001", "CHILLS", 1, "MODERATE", "VACC1", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1001", "CHILLS", 2, "MILD", "VACC1", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1001", "CHILLS", 3, "NONE", "VACC1", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1002", "CHILLS", 4, "MILD", "VACC2", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1002", "CHILLS", 6, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC",
+                        "XYZ1002", "CHILLS", 7, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC"
+                      )
+
+                      format_aval <- function(x) {
+                        case_when(
+                          x == "NONE" ~ 0,
+                          x == "MILD" ~ 1,
+                          x == "MODERATE" ~ 2,
+                          x == "SEVERE" ~ 3
+                        )}
+                      expected1 <- input %>%
+                        mutate(AVAL = format_aval(AVALC)) %>%
+                        arrange(USUBJID, FATESTCD, FATEST, FASCAT, FAOBJ, ATPTREF, AVAL) %>%
+                        group_by(USUBJID, FASCAT, FATESTCD, FATEST, FAOBJ, ATPTREF) %>%
+                        slice_tail(n = 1) %>%
+                        filter(FATESTCD == "SEV" & FAOBJ != "REDNESS") %>%
+                        mutate(DTYPE = "MAXIMUM", FATESTCD = "MAXSEV", FATEST = "Maximum severity")
+                      # expected dataset
+                      expected <- bind_rows(input %>% mutate(AVAL = format_aval(AVALC)), expected1)
 
 
-  expected1 <- input %>%
-    mutate(AVAL = format_aval(AVALC)) %>%
-    arrange(USUBJID, FATESTCD, FATEST, FASCAT, FAOBJ, ATPTREF, AVAL) %>%
-    group_by(USUBJID, FASCAT, FATESTCD, FATEST, FAOBJ, ATPTREF) %>%
-    slice_tail(n = 1) %>%
-    filter(FATESTCD == "SEV" & FAOBJ != "REDNESS") %>%
-    mutate(DTYPE = "MAXIMUM", FATESTCD = "MAXSEV", FATEST = "Maximum severity")
-  # expected dataset
-  expected <- bind_rows(input %>% mutate(AVAL = format_aval(AVALC)), expected1)
-
-
-  # actual dataset
-  actual <- derive_param_maxsev(
-    dataset = input,
-    filter_sev = "SEV",
-    exclude_events = "REDNESS",
-    test_maxsev = "Maximum severity",
-    testcd_maxsev = "MAXSEV",
-    by_vars = exprs(USUBJID, FAOBJ, ATPTREF)
-  )
-  expect_dfs_equal(actual,
-    expected,
-    keys = c(
-      "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
-      "ATPTREF", "AVALC", "AVAL"
-    )
-  )
-})
+                      # actual dataset
+                      actual <- derive_param_maxsev(
+                        dataset = input,
+                        filter_sev = "SEV",
+                        exclude_events = "REDNESS",
+                        test_maxsev = "Maximum severity",
+                        testcd_maxsev = "MAXSEV",
+                        by_vars = exprs(USUBJID, FAOBJ, ATPTREF)
+                      )
+                      expect_dfs_equal(actual,
+                                       expected,
+                                       keys = c(
+                                         "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
+                                         "ATPTREF", "AVALC", "AVAL"
+                                       )
+                      )
+                    })
 
 
 # test case 3 -------------------------------------------------------------
@@ -134,7 +138,13 @@ testthat::test_that("derive_param_maxsev Test 3: Checking whether the
     "XYZ1002", "CHILLS", 6, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC",
     "XYZ1002", "CHILLS", 7, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC"
   )
-
+  format_aval <- function(x) {
+    case_when(
+      x == "NONE" ~ 0,
+      x == "MILD" ~ 1,
+      x == "MODERATE" ~ 2,
+      x == "SEVERE" ~ 3
+    )}
 
   expected1 <- input %>%
     mutate(AVAL = format_aval(AVALC)) %>%
@@ -159,170 +169,10 @@ testthat::test_that("derive_param_maxsev Test 3: Checking whether the
 
   # testthat
   expect_dfs_equal(actual,
-    expected,
-    keys = c(
-      "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
-      "ATPTREF", "AVALC", "AVAL"
-    )
-  )
-})
-
-
-
-# test case 4 -------------------------------------------------------------
-
-testthat::test_that("derive_param_maxsev Test 4: Checking when exclude event is
-                    null", {
-  # input data
-  input <- tribble(
-    ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF, ~FATEST, ~FATESTCD, ~FASCAT,
-    "XYZ1001", "CHILLS", 2, "MILD", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1001", "CHILLS", 3, "NONE", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 4, "MILD", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 6, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 7, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC"
-  )
-
-
-  expected1 <- input %>%
-    mutate(AVAL = format_aval(AVALC)) %>%
-    arrange(USUBJID, FATESTCD, FATEST, FASCAT, FAOBJ, ATPTREF, AVAL) %>%
-    group_by(USUBJID, FASCAT, FATESTCD, FATEST, FAOBJ, ATPTREF) %>%
-    slice_tail(n = 1) %>%
-    filter(FATESTCD == "SEV") %>%
-    mutate(DTYPE = "MAXIMUM", FATESTCD = "MAXSEV", FATEST = "Maximum severity")
-  # expected dataset
-  expected <- bind_rows(input %>% mutate(AVAL = format_aval(AVALC)), expected1)
-
-
-  # actual dataset
-  actual <- derive_param_maxsev(
-    dataset = input,
-    filter_sev = "SEV",
-    exclude_events = NULL,
-    test_maxsev = "Maximum severity",
-    testcd_maxsev = "MAXSEV",
-    by_vars = exprs(USUBJID, FAOBJ, ATPTREF)
-  )
-  expect_dfs_equal(actual,
-    expected,
-    keys = c(
-      "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
-      "ATPTREF", "AVALC", "AVAL"
-    )
-  )
-})
-
-
-# test case 5 -------------------------------------------------------------
-
-testthat::test_that("derive_param_maxsev Test 5: Checking whether the variables
-which is listed in the retained_vars are retained and populated for
-                    derived records", {
-  # input data
-  input <- tribble(
-    ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF, ~FATEST, ~FATESTCD, ~FASCAT,
-    "XYZ1001", "REDNESS", 1, "MILD", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 2, "MODERATE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 3, "SEVERE", "VACC1", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "REDNESS", 0, "NONE", "VACC2", "Severity", "SEV", "ADMIN-SITE",
-    "XYZ1001", "CHILLS", 1, "MODERATE", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1001", "CHILLS", 2, "MILD", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1001", "CHILLS", 3, "NONE", "VACC1", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 4, "MILD", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 6, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC",
-    "XYZ1002", "CHILLS", 7, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC"
-  ) %>%
-    mutate(
-      EXTRT = "TRT1",
-      EXDOSU = "mg"
-    )
-
-
-  expected1 <- input %>%
-    mutate(AVAL = format_aval(AVALC)) %>%
-    arrange(USUBJID, FATESTCD, FATEST, FASCAT, FAOBJ, ATPTREF, AVAL) %>%
-    group_by(USUBJID, FASCAT, FATESTCD, FATEST, FAOBJ, ATPTREF) %>%
-    slice_tail(n = 1) %>%
-    filter(FATESTCD == "SEV") %>%
-    mutate(DTYPE = "MAXIMUM", FATESTCD = "MAXSEV", FATEST = "Maximum severity")
-  # expected dataset
-  expected <- bind_rows(input %>% mutate(AVAL = format_aval(AVALC)), expected1)
-
-
-  # actual dataset
-  actual <- derive_param_maxsev(
-    dataset = input,
-    filter_sev = "SEV",
-    exclude_events = NULL,
-    test_maxsev = "Maximum severity",
-    testcd_maxsev = "MAXSEV",
-    by_vars = exprs(USUBJID, FAOBJ, ATPTREF, EXTRT)
-  )
-
-  # testthat
-  expect_dfs_equal(actual,
-    expected,
-    keys = c(
-      "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
-      "ATPTREF", "AVALC", "AVAL"
-    )
-  )
-})
-
-
-# test case 6 -------------------------------------------------------------
-
-testthat::test_that("derive_param_maxsev Test 6: Checking whether the variables
-which is not listed in the retained_vars are not populated for the
-summary records but retained", {
-  # input data
-  input <- tribble(
-    ~USUBJID, ~FAOBJ, ~AVAL, ~AVALC, ~ATPTREF, ~FATEST, ~FATESTCD, ~FASCAT, ~SRCDOM,
-    "XYZ1001", "REDNESS", 1, "MILD", "VACC1", "Severity", "SEV", "ADMIN-SITE", "FA",
-    "XYZ1001", "REDNESS", 2, "MODERATE", "VACC1", "Severity", "SEV", "ADMIN-SITE", "FA",
-    "XYZ1001", "REDNESS", 3, "SEVERE", "VACC1", "Severity", "SEV", "ADMIN-SITE", "FA",
-    "XYZ1001", "REDNESS", 0, "NONE", "VACC2", "Severity", "SEV", "ADMIN-SITE", "FA",
-    "XYZ1001", "CHILLS", 1, "MODERATE", "VACC1", "Severity", "SEV", "SYSTEMIC", "FA",
-    "XYZ1001", "CHILLS", 2, "MILD", "VACC1", "Severity", "SEV", "SYSTEMIC", "FA",
-    "XYZ1001", "CHILLS", 3, "NONE", "VACC1", "Severity", "SEV", "SYSTEMIC", "FA",
-    "XYZ1002", "CHILLS", 4, "MILD", "VACC2", "Severity", "SEV", "SYSTEMIC", "FA",
-    "XYZ1002", "CHILLS", 6, "NONE", "VACC2", "Severity", "SEV", "SYSTEMIC", "FA",
-    "XYZ1002", "CHILLS", 7, "MODERATE", "VACC2", "Severity", "SEV", "SYSTEMIC", "FA"
-  ) %>%
-    mutate(
-      EXTRT = "TRT1",
-      EXDOSU = "mg"
-    )
-
-
-  expected1 <- input %>%
-    mutate(AVAL = format_aval(AVALC)) %>%
-    arrange(USUBJID, FATESTCD, FATEST, FASCAT, FAOBJ, ATPTREF, AVAL) %>%
-    group_by(USUBJID, FASCAT, FATESTCD, FATEST, FAOBJ, ATPTREF) %>%
-    slice_tail(n = 1) %>%
-    filter(FATESTCD == "SEV") %>%
-    mutate(DTYPE = "MAXIMUM", FATESTCD = "MAXSEV", FATEST = "Maximum severity")
-  # expected dataset
-  expected <- bind_rows(input %>% mutate(AVAL = format_aval(AVALC)), expected1)
-
-
-  # actual dataset
-  actual <- derive_param_maxsev(
-    dataset = input,
-    filter_sev = "SEV",
-    exclude_events = NULL,
-    test_maxsev = "Maximum severity",
-    testcd_maxsev = "MAXSEV",
-    by_vars = exprs(USUBJID, FAOBJ, ATPTREF, EXTRT)
-  )
-
-  # testthat
-  expect_dfs_equal(actual,
-    expected,
-    keys = c(
-      "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
-      "ATPTREF", "AVALC", "AVAL"
-    )
+                   expected,
+                   keys = c(
+                     "USUBJID", "FASCAT", "FATESTCD", "FATEST", "FAOBJ",
+                     "ATPTREF", "AVALC", "AVAL"
+                   )
   )
 })
