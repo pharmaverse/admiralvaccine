9808db485b0f1aed75ea6a15a7a3888a9d53bcb7
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-03-15 17:39:09 +0530

    Update admiralvaccine.R

--- R/admiralvaccine.R
+++ R/admiralvaccine.R
@@ -4,7 +4,7 @@
 #' @importFrom dplyr arrange bind_rows case_when desc ends_with filter full_join group_by
 #'             if_else mutate mutate_at mutate_if n pull rename rename_at row_number select slice
 #'             starts_with transmute ungroup vars n_distinct union distinct
-#'             summarise_at summarise coalesce bind_cols na_if any_of tibble
+#'             summarise_at summarise coalesce bind_cols na_if tibble
 #' @importFrom magrittr %>%
 #' @importFrom rlang := abort arg_match as_function as_string call2 caller_env
 #'             call_name current_env .data enexpr enquo eval_bare eval_tidy expr
@@ -13,12 +13,13 @@
 #'             is_logical is_quosure is_quosures is_symbol new_formula
 #'             parse_expr parse_exprs quo quo_get_expr quo_is_call
 #'             quo_is_missing quo_is_null quo_is_symbol quos quo_squash quo_text
-#'             set_names sym syms type_of warn quo_set_env quo_get_env exprs
+#'             set_names sym syms type_of warn quo_set_env quo_get_env
 #' @importFrom utils capture.output str
 #' @importFrom purrr map map2 map_chr map_lgl reduce walk keep map_if transpose
 #'             flatten every modify_at modify_if reduce compose
 #' @importFrom stringr str_c str_detect str_extract str_remove str_remove_all
 #'             str_replace str_trim str_to_lower str_to_title str_to_upper str_glue
+#'             str_to_sentence
 #' @importFrom assertthat assert_that is.number on_failure<-
 #' @importFrom lubridate as_datetime ceiling_date date days duration floor_date is.Date is.instant
 #'             time_length %--% ymd ymd_hms weeks years hours minutes
@@ -28,5 +29,6 @@
 #' @importFrom lifecycle deprecate_warn deprecated deprecate_stop
 #' @importFrom admiral derive_vars_dy
 #' @importFrom admiraldev assert_logical_scalar assert_character_vector assert_vars
-#'             assert_data_frame assert_character_scalar
+#'             assert_data_frame assert_numeric_vector assert_character_scalar
+#'
 "_PACKAGE"
