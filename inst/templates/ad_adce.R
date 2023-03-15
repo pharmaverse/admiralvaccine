d1e2bded7d8717c2f6e9776cec59ff6569c905ba
Armishaw <leearmishaw@gmail.com>
2023-03-15 04:02:02 -0400

    Adjusted code for lintr

--- inst/templates/ad_adce.R
+++ inst/templates/ad_adce.R
@@ -20,20 +20,20 @@ ce <- read_sas("./data/ce.sas7bdat")
 # When SAS datasets are imported into R using haven::read_sas(), missing
 # character values from SAS appear as "" characters in R, instead of appearing
 # as NA values. Further details can be obtained via the following link:
-# https://pharmaverse.github.io/admiral/articles/admiral.html#handling-of-missing-values
+# https://pharmaverse.github.io/admiral/articles/admiral.html#handling-of-missing-values # nolint
 
 ce <- convert_blanks_to_na(ce)
 
 # Derivations ----
 # Get CE records
 ce01 <- ce %>%
-  filter(CECAT == "REACTOGENICITY") # & CEOCCUR=="Y" & !is.na(CETOXGR) & CEGRPID != "IMMEDIATE REACTION")
+  filter(CECAT == "REACTOGENICITY")
 
 # Get list of ADSL vars required for derivations
 adsl_vars <- exprs(TRTSDT, TRTEDT)
 
 # Create period dataset - for joining period information onto ce records
-# Need to remove datetime variables as otherwise causes duplicate issues - to be looked into?
+# Need to remove datetime variables as otherwise causes duplicate issues
 adsl2 <- adsl %>%
   select(-c(AP01SDTM, AP01EDTM, AP02SDTM, AP02EDTM, AP06SDTM, AP06EDTM))
 
@@ -90,7 +90,8 @@ ce03 <-
     ATOXGRN = as.integer(factor(ATOXGR, levels = c("0", "1", "2", "3", "4"))),
     # ASEVN = as.integer(factor(ASEV, levels = c("MILD", "MODERATE", "SEVERE", "DEATH THREATENING")))
   ) %>%
-  ## Derive occurrence flags: first occurrence of most severe solicited AE - Janssen specific
+  ## Derive occurrence flags: first occurrence of most severe solicited AE
+  ## - Janssen specific
   restrict_derivation(
     derivation = derive_var_extreme_flag,
     args = params(
