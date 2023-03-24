5409e52d6ccd6e93bf1daaf76163a8f8753386d1
vikrams95 <106981001+vikrams95@users.noreply.github.com>
2023-03-24 16:04:01 +0530

    Update test-derive_param_maxtemp.R

--- tests/testthat/test-derive_param_maxtemp.R
+++ tests/testthat/test-derive_param_maxtemp.R
@@ -5,7 +5,7 @@ library(diffdf)
 library(testthat)
 
 # derive_param_maxtemp
-test_that("derive mamimum temperature records",{
+test_that("derive mamimum temperature records", {
   input <- tribble(
     ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
     "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
@@ -20,19 +20,21 @@ test_that("derive mamimum temperature records",{
 
   expected1 <- input %>%
     filter(FAOBJ == "FEVER") %>%
-    arrange(USUBJID, FAOBJ, ATPTREF,AVALC,VSSTRESN,ATPTN) %>%
+    arrange(USUBJID, FAOBJ, ATPTREF, AVALC, VSSTRESN, ATPTN) %>%
     group_by(USUBJID, FAOBJ, ATPTREF) %>%
-    slice_tail(n=1) %>%
-    mutate(AVAL=VSSTRESN,
-           AVALC=as.character(AVAL),
-           DTYPE='MAXIMUM',
-           FATESTCD='MAXTEMP',
-           FATEST='Maximum temperature') %>%
-    select(USUBJID, FAOBJ,ATPTREF,AVALC,FATEST,FATESTCD,AVAL,DTYPE)
+    slice_tail(n = 1) %>%
+    mutate(
+      AVAL = VSSTRESN,
+      AVALC = as.character(AVAL),
+      DTYPE = "MAXIMUM",
+      FATESTCD = "MAXTEMP",
+      FATEST = "Maximum temperature"
+    ) %>%
+    select(USUBJID, FAOBJ, ATPTREF, AVALC, FATEST, FATESTCD, AVAL, DTYPE)
 
-  expected_output <- bind_rows(input,expected1)
+  expected_output <- bind_rows(input, expected1)
 
-  actual_output <-derive_param_maxtemp(
+  actual_output <- derive_param_maxtemp(
     input,
     filter_faobj = "FEVER",
     by_vars = exprs(USUBJID, FAOBJ, ATPTREF),
@@ -43,141 +45,52 @@ test_that("derive mamimum temperature records",{
   expect_dfs_equal(
     expected_output,
     actual_output,
-    keys = c('USUBJID','FAOBJ','AVAL','AVALC','ATPTREF','FATEST',
-             'FATESTCD','VSSTRESN','ATPTN')
+    keys = c(
+      "USUBJID", "FAOBJ", "AVAL", "AVALC", "ATPTREF", "FATEST",
+      "FATESTCD", "VSSTRESN", "ATPTN"
+    )
   )
 })
 
 # derive_param_maxtemp
 test_that("derive mamimum temperature records if more than one record has
-          same maximum value within by group ",{
-            input <- tribble(
-              ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
-              "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 1", "Y", 2, 39.2, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 1", "Y", 3, 39.2, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 2", "Y", 1, 37.3, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 2", "Y", 2, 36.2, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 2", "Y", 3, 37.3, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 1", "Y", 1, 38.2, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 1", "Y", 2, 37.5, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 1", "Y", 3, 36.5, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 2", "N", 1, 36.1, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.4, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.7, "Occurrence Indicator", "OCCUR"
-            )
-
-            expected1 <- input %>%
-              filter(FAOBJ == "FEVER") %>%
-              arrange(USUBJID, FAOBJ, ATPTREF,AVALC,VSSTRESN,ATPTN) %>%
-              group_by(USUBJID, FAOBJ, ATPTREF) %>%
-              slice_tail(n=1) %>%
-              mutate(AVAL=VSSTRESN,
-                     AVALC=as.character(AVAL),
-                     DTYPE='MAXIMUM',
-                     FATESTCD='MAXTEMP',
-                     FATEST='Maximum temperature') %>%
-              select(USUBJID, FAOBJ,ATPTREF,AVALC,FATEST,FATESTCD,AVAL,DTYPE)
-
-            expected_output <- bind_rows(input,expected1)
-
-            actual_output <-derive_param_maxtemp(
-              input,
-              filter_faobj = "FEVER",
-              by_vars = exprs(USUBJID, FAOBJ, ATPTREF),
-              test_maxtemp = "Maximum temperature",
-              testcd_maxtemp = "MAXTEMP"
-            )
-
-            expect_dfs_equal(
-              expected_output,
-              actual_output,
-              keys = c('USUBJID','FAOBJ','AVAL','AVALC','ATPTREF','FATEST',
-                       'FATESTCD','VSSTRESN','ATPTN')
-            )
-          })
-
-# derive_param_maxtemp
-test_that("Check if the argument `filter_faobj` filters only fever records
-          for deriving maximum temperature records",{
-            input <- tribble(
-              ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
-              "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 1", "Y", 2, 39.2, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 2", "Y", 1, 37.3, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "FEVER", "VACC 2", "Y", 2, 36.2, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 1", "Y", 1, 38.2, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 1", "Y", 2, 37.5, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 2", "N", 1, 36.1, "Occurrence Indicator", "OCCUR",
-              "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.4, "Occurrence Indicator", "OCCUR",
-              "XYZ1001", "REDNESS", "VACC 1", "3", 1, NA, "Diameter", "DIAMETER",
-              "XYZ1001", "REDNESS", "VACC 1", "7", 2, NA, "Diameter", "DIAMETER",
-              "XYZ1001", "REDNESS", "VACC 1", "5", 1, NA, "Diameter", "DIAMETER",
-              "XYZ1001", "REDNESS", "VACC 1", "6", 2, NA, "Diameter", "DIAMETER"
-            )
-
-            expected1 <- input %>%
-              filter(FAOBJ == "FEVER") %>%
-              arrange(USUBJID, FAOBJ, ATPTREF,AVALC,VSSTRESN,ATPTN) %>%
-              group_by(USUBJID, FAOBJ, ATPTREF) %>%
-              slice_tail(n=1) %>%
-              mutate(AVAL=VSSTRESN,
-                     AVALC=as.character(AVAL),
-                     DTYPE='MAXIMUM',
-                     FATESTCD='MAXTEMP',
-                     FATEST='Maximum temperature') %>%
-              select(USUBJID, FAOBJ,ATPTREF,AVALC,FATEST,FATESTCD,AVAL,DTYPE)
-
-            expected_output <- bind_rows(input,expected1)
-
-            actual_output <-derive_param_maxtemp(
-              input,
-              filter_faobj = "FEVER",
-              by_vars = exprs(USUBJID, FAOBJ, ATPTREF),
-              test_maxtemp = "Maximum temperature",
-              testcd_maxtemp = "MAXTEMP"
-            )
-
-            expect_dfs_equal(
-              expected_output,
-              actual_output,
-              keys = c('USUBJID','FAOBJ','AVAL','AVALC','ATPTREF','FATEST',
-                       'FATESTCD','VSSTRESN','ATPTN')
-            )
-          })
-
-# derive_param_maxtemp
-test_that("Change by group variables and check if the results are matching",{
+          same maximum value within by group ", {
   input <- tribble(
     ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
     "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
     "XYZ1001", "FEVER", "VACC 1", "Y", 2, 39.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 1", "Y", 3, 39.2, "Occurrence Indicator", "OCCUR",
     "XYZ1001", "FEVER", "VACC 2", "Y", 1, 37.3, "Occurrence Indicator", "OCCUR",
     "XYZ1001", "FEVER", "VACC 2", "Y", 2, 36.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 2", "Y", 3, 37.3, "Occurrence Indicator", "OCCUR",
     "XYZ1002", "FEVER", "VACC 1", "Y", 1, 38.2, "Occurrence Indicator", "OCCUR",
     "XYZ1002", "FEVER", "VACC 1", "Y", 2, 37.5, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 1", "Y", 3, 36.5, "Occurrence Indicator", "OCCUR",
     "XYZ1002", "FEVER", "VACC 2", "N", 1, 36.1, "Occurrence Indicator", "OCCUR",
-    "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.4, "Occurrence Indicator", "OCCUR"
+    "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.4, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.7, "Occurrence Indicator", "OCCUR"
   )
 
   expected1 <- input %>%
     filter(FAOBJ == "FEVER") %>%
-    arrange(USUBJID, FAOBJ, ATPTREF,AVALC,VSSTRESN,ATPTN) %>%
-    group_by(USUBJID,ATPTREF) %>%
-    slice_tail(n=1) %>%
-    mutate(AVAL=VSSTRESN,
-           AVALC=as.character(AVAL),
-           DTYPE='MAXIMUM',
-           FATESTCD='MAXTEMP',
-           FATEST='Maximum temperature') %>%
-    select(USUBJID, FAOBJ,ATPTREF,AVALC,FATEST,FATESTCD,AVAL,DTYPE)
+    arrange(USUBJID, FAOBJ, ATPTREF, AVALC, VSSTRESN, ATPTN) %>%
+    group_by(USUBJID, FAOBJ, ATPTREF) %>%
+    slice_tail(n = 1) %>%
+    mutate(
+      AVAL = VSSTRESN,
+      AVALC = as.character(AVAL),
+      DTYPE = "MAXIMUM",
+      FATESTCD = "MAXTEMP",
+      FATEST = "Maximum temperature"
+    ) %>%
+    select(USUBJID, FAOBJ, ATPTREF, AVALC, FATEST, FATESTCD, AVAL, DTYPE)
 
-  expected_output <- bind_rows(input,expected1)
+  expected_output <- bind_rows(input, expected1)
 
-  actual_output <-derive_param_maxtemp(
+  actual_output <- derive_param_maxtemp(
     input,
     filter_faobj = "FEVER",
-    by_vars = exprs(USUBJID,ATPTREF),
+    by_vars = exprs(USUBJID, FAOBJ, ATPTREF),
     test_maxtemp = "Maximum temperature",
     testcd_maxtemp = "MAXTEMP"
   )
@@ -185,13 +98,68 @@ test_that("Change by group variables and check if the results are matching",{
   expect_dfs_equal(
     expected_output,
     actual_output,
-    keys = c('USUBJID','FAOBJ','AVAL','AVALC','ATPTREF','FATEST',
-             'FATESTCD','VSSTRESN','ATPTN')
+    keys = c(
+      "USUBJID", "FAOBJ", "AVAL", "AVALC", "ATPTREF", "FATEST",
+      "FATESTCD", "VSSTRESN", "ATPTN"
+    )
   )
 })
 
 # derive_param_maxtemp
-test_that("Check if the arguments `test_maxtemp`,`testcd_maxtemp` works fine ",{
+test_that("Check if the argument `filter_faobj` filters only fever records
+          for deriving maximum temperature records", {
+  input <- tribble(
+    ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
+    "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 1", "Y", 2, 39.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 2", "Y", 1, 37.3, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 2", "Y", 2, 36.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 1", "Y", 1, 38.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 1", "Y", 2, 37.5, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 2", "N", 1, 36.1, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.4, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "REDNESS", "VACC 1", "3", 1, NA, "Diameter", "DIAMETER",
+    "XYZ1001", "REDNESS", "VACC 1", "7", 2, NA, "Diameter", "DIAMETER",
+    "XYZ1001", "REDNESS", "VACC 1", "5", 1, NA, "Diameter", "DIAMETER",
+    "XYZ1001", "REDNESS", "VACC 1", "6", 2, NA, "Diameter", "DIAMETER"
+  )
+
+  expected1 <- input %>%
+    filter(FAOBJ == "FEVER") %>%
+    arrange(USUBJID, FAOBJ, ATPTREF, AVALC, VSSTRESN, ATPTN) %>%
+    group_by(USUBJID, FAOBJ, ATPTREF) %>%
+    slice_tail(n = 1) %>%
+    mutate(
+      AVAL = VSSTRESN,
+      AVALC = as.character(AVAL),
+      DTYPE = "MAXIMUM",
+      FATESTCD = "MAXTEMP",
+      FATEST = "Maximum temperature"
+    ) %>%
+    select(USUBJID, FAOBJ, ATPTREF, AVALC, FATEST, FATESTCD, AVAL, DTYPE)
+
+  expected_output <- bind_rows(input, expected1)
+
+  actual_output <- derive_param_maxtemp(
+    input,
+    filter_faobj = "FEVER",
+    by_vars = exprs(USUBJID, FAOBJ, ATPTREF),
+    test_maxtemp = "Maximum temperature",
+    testcd_maxtemp = "MAXTEMP"
+  )
+
+  expect_dfs_equal(
+    expected_output,
+    actual_output,
+    keys = c(
+      "USUBJID", "FAOBJ", "AVAL", "AVALC", "ATPTREF", "FATEST",
+      "FATESTCD", "VSSTRESN", "ATPTN"
+    )
+  )
+})
+
+# derive_param_maxtemp
+test_that("Change by group variables and check if the results are matching", {
   input <- tribble(
     ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
     "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
@@ -206,22 +174,72 @@ test_that("Check if the arguments `test_maxtemp`,`testcd_maxtemp` works fine ",{
 
   expected1 <- input %>%
     filter(FAOBJ == "FEVER") %>%
-    arrange(USUBJID, FAOBJ, ATPTREF,AVALC,VSSTRESN,ATPTN) %>%
-    group_by(USUBJID,ATPTREF) %>%
-    slice_tail(n=1) %>%
-    mutate(AVAL=VSSTRESN,
-           AVALC=as.character(AVAL),
-           DTYPE='MAXIMUM',
-           FATESTCD='MAXTEMPERATURE',
-           FATEST='Maximum temp') %>%
-    select(USUBJID, FAOBJ,ATPTREF,AVALC,FATEST,FATESTCD,AVAL,DTYPE)
+    arrange(USUBJID, FAOBJ, ATPTREF, AVALC, VSSTRESN, ATPTN) %>%
+    group_by(USUBJID, ATPTREF) %>%
+    slice_tail(n = 1) %>%
+    mutate(
+      AVAL = VSSTRESN,
+      AVALC = as.character(AVAL),
+      DTYPE = "MAXIMUM",
+      FATESTCD = "MAXTEMP",
+      FATEST = "Maximum temperature"
+    ) %>%
+    select(USUBJID, FAOBJ, ATPTREF, AVALC, FATEST, FATESTCD, AVAL, DTYPE)
 
-  expected_output <- bind_rows(input,expected1)
+  expected_output <- bind_rows(input, expected1)
 
-  actual_output <-derive_param_maxtemp(
+  actual_output <- derive_param_maxtemp(
     input,
     filter_faobj = "FEVER",
-    by_vars = exprs(USUBJID,ATPTREF),
+    by_vars = exprs(USUBJID, ATPTREF),
+    test_maxtemp = "Maximum temperature",
+    testcd_maxtemp = "MAXTEMP"
+  )
+
+  expect_dfs_equal(
+    expected_output,
+    actual_output,
+    keys = c(
+      "USUBJID", "FAOBJ", "AVAL", "AVALC", "ATPTREF", "FATEST",
+      "FATESTCD", "VSSTRESN", "ATPTN"
+    )
+  )
+})
+
+# derive_param_maxtemp
+test_that("Check if the arguments `test_maxtemp`,`testcd_maxtemp` works fine ", {
+  input <- tribble(
+    ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
+    "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 1", "Y", 2, 39.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 2", "Y", 1, 37.3, "Occurrence Indicator", "OCCUR",
+    "XYZ1001", "FEVER", "VACC 2", "Y", 2, 36.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 1", "Y", 1, 38.2, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 1", "Y", 2, 37.5, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 2", "N", 1, 36.1, "Occurrence Indicator", "OCCUR",
+    "XYZ1002", "FEVER", "VACC 2", "N", 2, 36.4, "Occurrence Indicator", "OCCUR"
+  )
+
+  expected1 <- input %>%
+    filter(FAOBJ == "FEVER") %>%
+    arrange(USUBJID, FAOBJ, ATPTREF, AVALC, VSSTRESN, ATPTN) %>%
+    group_by(USUBJID, ATPTREF) %>%
+    slice_tail(n = 1) %>%
+    mutate(
+      AVAL = VSSTRESN,
+      AVALC = as.character(AVAL),
+      DTYPE = "MAXIMUM",
+      FATESTCD = "MAXTEMPERATURE",
+      FATEST = "Maximum temp"
+    ) %>%
+    select(USUBJID, FAOBJ, ATPTREF, AVALC, FATEST, FATESTCD, AVAL, DTYPE)
+
+  expected_output <- bind_rows(input, expected1)
+
+  actual_output <- derive_param_maxtemp(
+    input,
+    filter_faobj = "FEVER",
+    by_vars = exprs(USUBJID, ATPTREF),
     test_maxtemp = "Maximum temp",
     testcd_maxtemp = "MAXTEMPERATURE"
   )
@@ -229,14 +247,16 @@ test_that("Check if the arguments `test_maxtemp`,`testcd_maxtemp` works fine ",{
   expect_dfs_equal(
     expected_output,
     actual_output,
-    keys = c('USUBJID','FAOBJ','AVAL','AVALC','ATPTREF','FATEST',
-             'FATESTCD','VSSTRESN','ATPTN')
+    keys = c(
+      "USUBJID", "FAOBJ", "AVAL", "AVALC", "ATPTREF", "FATEST",
+      "FATESTCD", "VSSTRESN", "ATPTN"
+    )
   )
 })
 
 # derive_param_maxtemp
 test_that("Check if getting the expected error while passing invalid faobj
-          in filter_faobj argument ",{
+          in filter_faobj argument ", {
   input <- tribble(
     ~USUBJID, ~FAOBJ, ~ATPTREF, ~AVALC, ~ATPTN, ~VSSTRESN, ~FATEST, ~FATESTCD,
     "XYZ1001", "FEVER", "VACC 1", "Y", 1, 38.9, "Occurrence Indicator", "OCCUR",
@@ -250,15 +270,15 @@ test_that("Check if getting the expected error while passing invalid faobj
   )
 
   expect_error(
-    actual_output <-derive_param_maxtemp(
+    actual_output <- derive_param_maxtemp(
       input,
       filter_faobj = "REDNESS",
-      by_vars = exprs(USUBJID,ATPTREF),
+      by_vars = exprs(USUBJID, ATPTREF),
       test_maxtemp = "Maximum temp",
       testcd_maxtemp = "MAXTEMPERATURE"
     ),
     regexp = paste(
-      "REDNESS doesn't exist in the FAOBJ variable.")
+      "REDNESS doesn't exist in the FAOBJ variable."
+    )
   )
-
 })
