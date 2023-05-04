1fbe84a0f6007b75e432c6657e9199ddaf901094
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-05-04 21:12:33 +0530

    Update derive_vars_merged_vaccine.R

--- R/derive_vars_merged_vaccine.R
+++ R/derive_vars_merged_vaccine.R
@@ -55,7 +55,7 @@
 #'   dataset_ex = vx_ex,
 #'   dataset_supp = NULL,
 #'   dataset_suppex = NULL,
-#'   ex_vars = exprs(EXTRT, EXDOSE, EXDOSEU, EXSTDTC, EXENDTC)
+#'   ex_vars = exprs(EXTRT, EXDOSE, EXDOSU, EXSTDTC, EXENDTC)
 #' )
 #'
 derive_vars_merged_vaccine <- function(dataset,
