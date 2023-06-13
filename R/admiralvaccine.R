3d7f78c392d842f8969b042cdc3e883834c73659
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-06-13 14:56:36 +0530

    Update admiralvaccine.R

--- R/admiralvaccine.R
+++ R/admiralvaccine.R
@@ -4,9 +4,8 @@
 #' @importFrom dplyr arrange bind_rows case_when desc ends_with filter full_join group_by
 #'             if_else mutate mutate_at mutate_if n pull rename rename_at row_number select slice
 #'             starts_with transmute ungroup vars n_distinct union distinct
-#'             summarise_at summarise coalesce bind_cols na_if any_of tibble first left_join
+#'             summarise_at summarise coalesce bind_cols na_if any_of tibble %>% first left_join
 #'             between slice_tail
-#' @importFrom magrittr %>%
 #' @importFrom rlang := abort arg_match as_function as_string call2 caller_env
 #'             call_name current_env .data enexpr enquo eval_bare eval_tidy expr
 #'             expr_interp expr_label f_lhs f_rhs inform
