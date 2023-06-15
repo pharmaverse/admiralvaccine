c33125186a76746ff296446f5956913260000869
vikrams95 <106981001+vikrams95@users.noreply.github.com>
2023-06-15 16:20:34 +0530

    #192 final updates

--- tests/testthat/test-derive_vars_params.R
+++ tests/testthat/test-derive_vars_params.R
@@ -1,6 +1,7 @@
-## Test 1: checking
+## Test 1: Check if the variables in the lookup dataset getting merged properly
 
-test_that("derive_vars_params Test 1: checking", {
+test_that("derive_vars_params Test 1: Check if the variables in the lookup dataset getting merged
+           properly", {
   lookup_dataset <- tibble::tribble(
     ~FATESTCD,    ~PARAMCD,   ~FAOBJ,
     "SEV",        "SEVREDN",  "Redness",
@@ -25,8 +26,6 @@ test_that("derive_vars_params Test 1: checking", {
     "ABC101", "REACTOGENICITY", "ADMIN-SITE", "SEV", "Swelling", "Severity",
     "ABC101", "REACTOGENICITY", "ADMIN-SITE", "OCCUR", "Swelling", "Occurrence"
   )
-
-
 
   expout1 <- input %>%
     left_join(lookup_dataset,
