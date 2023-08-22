04c56515026f1aba468418d73b1ae5bb5770ae14
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-08-23 01:32:50 +0530

    #218

---
+++ inst/create_vx_data/vx_suppce.R
@@ -0,0 +1,49 @@
+
+library(tibble)
+library(tidyverse)
+
+
+#suppce
+suppce <- tribble(
+  ~STUDYID,
+  "ABC"
+)
+
+vx_suppce <- suppce %>% mutate(STUDYID = "ABC",
+                            USUBJID = "ABC-1001",
+                            RDOMAIN = "CE",
+                            IDVAR = "CESEQ",
+                            IDVARVAL = "1",
+                            QNAM = "CEEVAL",
+                            QVAL = "STUDY SUBJECT",
+                            QLABEL = "Evaluator",
+                            QORIG = "ASSIGNED") %>%
+  add_row(STUDYID = "ABC",
+          USUBJID = "ABC-1001",
+          RDOMAIN = "CE",
+          IDVAR = "CESEQ",
+          IDVARVAL = "2",
+          QNAM = "CEEVAL",
+          QVAL = "STUDY SUBJECT",
+          QLABEL = "Evaluator",
+          QORIG = "ASSIGNED")%>%
+  add_row(STUDYID = "ABC",
+          USUBJID = "ABC-1002",
+          RDOMAIN = "CE",
+          IDVAR = "CESEQ",
+          IDVARVAL = "1",
+          QNAM = "CEEVAL",
+          QVAL = "STUDY SUBJECT",
+          QLABEL = "Evaluator",
+          QORIG = "ASSIGNED")%>%
+  add_row(STUDYID = "ABC",
+          USUBJID = "ABC-1002",
+          RDOMAIN = "CE",
+          IDVAR = "CESEQ",
+          IDVARVAL = "2",
+          QNAM = "CEEVAL",
+          QVAL = "STUDY SUBJECT",
+          QLABEL = "Evaluator",
+          QORIG = "ASSIGNED")
+dir <- tempdir()
+save(vx_suppce, file = file.path(dir, "vx_suppce.rda"), compress = "bzip2")
