361bd0e7d31bce2e67612b96c4e6bce584396d92
Arjun Rubalingam <arjun.ruba@gmail.com>
2023-08-23 01:32:57 +0530

    #218

---
+++ inst/create_vx_data/vx_suppdm.R
@@ -0,0 +1,29 @@
+library(tibble)
+library(tidyverse)
+#suppdm
+suppdm <- tribble(
+  ~STUDYID,
+  "ABC"
+)
+
+vx_suppdm <- suppdm %>% mutate(STUDYID = "ABC",
+                            USUBJID = "ABC-1001",
+                            RDOMAIN = "DM",
+                            IDVAR = "",
+                            IDVARVAL = "",
+                            QNAM = "RACIALD",
+                            QVAL = "OTHER",
+                            QLABEL = "Racial Designation",
+                            QORIG = "CRF") %>%
+  add_row(STUDYID = "ABC",
+          USUBJID = "ABC-1002",
+          RDOMAIN = "DM",
+          IDVAR = "",
+          IDVARVAL = "",
+          QNAM = "RACIALD",
+          QVAL = "OTHER",
+          QLABEL = "Racial Designation",
+          QORIG = "CRF")
+
+dir <- tempdir()
+save(vx_suppdm, file = file.path(dir, "vx_suppdm.rda"), compress = "bzip2")
