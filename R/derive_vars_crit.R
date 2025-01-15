5623e152b9f0b42e69b9c445a394314abef994e4
Federico Baratin <federico.x.baratin@gsk.com>
2025-01-15 15:41:47 +0100

    Updated function as per deprecation strategy, Phase 1.

--- R/derive_vars_crit.R
+++ R/derive_vars_crit.R
@@ -45,17 +45,100 @@
 #'
 #' @author Federico Baratin
 #' @export
-#' @keywords deprecated
-#' @family deprecated
+#' @keywords der_var
+#' @family der_var
+#'
+#' @examples
+#' library(tibble)
+#' library(admiral)
+#' library(admiraldev)
+#' library(dplyr)
+#'
+#' input <- tribble(
+#'   ~USUBJID, ~AVISITN, ~ISCAT, ~PARAMCD, ~AVAL, ~ISLLOQ,
+#'   "999999-000001", 10, "IMMUNOLOGY", "J0033VN", 2, 4,
+#'   "999999-000001", 10, "IMMUNOLOGY", "I0019NT", 3, 6,
+#'   "999999-000001", 10, "IMMUNOLOGY", "M0019LN", 4, 4,
+#'   "999999-000001", 10, "IMMUNOLOGY", "R0003MA", 3, 6,
+#'   "999999-000001", 30, "IMMUNOLOGY", "J0033VN", 60, 4,
+#'   "999999-000001", 30, "IMMUNOLOGY", "I0019NT", 567, 6,
+#'   "999999-000001", 30, "IMMUNOLOGY", "M0019LN", 659, 4,
+#'   "999999-000001", 30, "IMMUNOLOGY", "R0003MA", 250, 6,
+#'   "999999-000002", 10, "IMMUNOLOGY", "J0033VN", 2, 4,
+#'   "999999-000002", 10, "IMMUNOLOGY", "I0019NT", 7, 6,
+#'   "999999-000002", 10, "IMMUNOLOGY", "M0019LN", 5, 4,
+#'   "999999-000002", 10, "IMMUNOLOGY", "R0003MA", 3, 6,
+#'   "999999-000002", 30, "IMMUNOLOGY", "J0033VN", 55, 4,
+#'   "999999-000002", 30, "IMMUNOLOGY", "I0019NT", 89, 6,
+#'   "999999-000002", 30, "IMMUNOLOGY", "M0019LN", 990, 4,
+#'   "999999-000002", 30, "IMMUNOLOGY", "R0003MA", 340, 6,
+#'   "999999-000003", 10, "IMMUNOLOGY", "J0033VN", 3, 4,
+#'   "999999-000003", 10, "IMMUNOLOGY", "I0019NT", 6, 6,
+#'   "999999-000003", 10, "IMMUNOLOGY", "M0019LN", 2, 4,
+#'   "999999-000003", 10, "IMMUNOLOGY", "R0003MA", 2, 6,
+#'   "999999-000003", 30, "IMMUNOLOGY", "J0033VN", 45, 4,
+#'   "999999-000003", 30, "IMMUNOLOGY", "I0019NT", 381, 6,
+#'   "999999-000003", 30, "IMMUNOLOGY", "M0019LN", 542, 4,
+#'   "999999-000003", 30, "IMMUNOLOGY", "R0003MA", NA, 6
+#' )
+#'
+#'
+#' derive_vars_crit(
+#'   dataset = input,
+#'   prefix = "CRIT1",
+#'   crit_label = "Titer >= ISLLOQ",
+#'   condition = !is.na(AVAL) & !is.na(ISLLOQ),
+#'   criterion = AVAL >= ISLLOQ
+#' )
 #'
 derive_vars_crit <- function(dataset, prefix, crit_label, condition, criterion) {
-  deprecate_warn("0.4.0", "derive_vars_crit()", "admiral::derive_vars_crit_flag()")
-  admiral::derive_vars_crit_flag(
-    dataset = dataset,
-    crit_nr = 1,
-    condition = !!criterion,
-    description = !!crit_label,
-    values_yn = TRUE,
-    create_numeric_flag = TRUE
-  )
+  condition <- assert_filter_cond(enquo(condition))
+  criterion <- assert_filter_cond(enquo(criterion))
+
+  var_char <- paste0(prefix, "FL")
+  var_num <- paste0(prefix, "FN")
+
+  if (grepl("CRIT", prefix)) {
+    data <- dataset %>%
+      mutate(
+        `:=`(
+          !!var_char,
+          case_when(
+            !(!!condition) ~ NA_character_,
+            !!criterion & !!condition ~ "Y",
+            !(!!criterion) & !!condition ~ "N"
+          )
+        ),
+        `:=`(
+          !!var_num,
+          case_when(
+            !(!!condition) ~ NA_real_,
+            !!criterion & !!condition ~ 1,
+            !(!!criterion) & !!condition ~ 0
+          )
+        ),
+        `:=`(
+          !!prefix,
+          case_when(
+            !(!!condition) ~ NA_character_,
+            !!criterion & !!condition ~ crit_label,
+            !(!!criterion) & !!condition ~ crit_label
+          )
+        )
+      )
+  } else {
+    data <- dataset %>%
+      mutate(
+        `:=`(
+          !!var_char,
+          case_when(
+            !(!!condition) ~ NA_character_,
+            !!criterion & !!condition ~ "Y",
+            !(!!criterion) & !!condition ~ "N"
+          )
+        )
+      )
+  }
+
+  return(data)
 }
