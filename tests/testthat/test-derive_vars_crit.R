959f2d2eef65080738fdd019d876ec26db0e20b9
ahasoplakus <sukalposaha@gmail.com>
2025-01-28 10:41:56 +0000

    style fix

--- tests/testthat/test-derive_vars_crit.R
+++ tests/testthat/test-derive_vars_crit.R
@@ -1,48 +1,48 @@
 ## Test 1: deprecation message if function is called
 
 test_that("derive_vars_crit() Test #: deprecation message if function is called", {
-    input <- tibble::tribble(
-      ~USUBJID, ~AVISITN, ~PARAMCD, ~AVAL, ~ISLLOQ,
-      "999999-000001", 10, "J0033VN", 2, 4,
-      "999999-000001", 10, "I0019NT", 3, 6,
-      "999999-000001", 10, "M0019LN", 4, 4,
-      "999999-000001", 10, "R0003MA", 3, 6,
-      "999999-000001", 30, "J0033VN", 60, 4,
-      "999999-000001", 30, "I0019NT", 567, 6,
-      "999999-000001", 30, "M0019LN", 659, 4,
-      "999999-000001", 30, "R0003MA", 250, 6,
-      "999999-000002", 10, "J0033VN", 2, 4,
-      "999999-000002", 10, "I0019NT", 7, 6,
-      "999999-000002", 10, "M0019LN", 5, 4,
-      "999999-000002", 10, "R0003MA", 3, 6,
-      "999999-000002", 30, "J0033VN", 55, 4,
-      "999999-000002", 30, "I0019NT", 89, 6,
-      "999999-000002", 30, "M0019LN", 990, 4,
-      "999999-000002", 30, "R0003MA", 340, 6
-    )
+  input <- tibble::tribble(
+    ~USUBJID, ~AVISITN, ~PARAMCD, ~AVAL, ~ISLLOQ,
+    "999999-000001", 10, "J0033VN", 2, 4,
+    "999999-000001", 10, "I0019NT", 3, 6,
+    "999999-000001", 10, "M0019LN", 4, 4,
+    "999999-000001", 10, "R0003MA", 3, 6,
+    "999999-000001", 30, "J0033VN", 60, 4,
+    "999999-000001", 30, "I0019NT", 567, 6,
+    "999999-000001", 30, "M0019LN", 659, 4,
+    "999999-000001", 30, "R0003MA", 250, 6,
+    "999999-000002", 10, "J0033VN", 2, 4,
+    "999999-000002", 10, "I0019NT", 7, 6,
+    "999999-000002", 10, "M0019LN", 5, 4,
+    "999999-000002", 10, "R0003MA", 3, 6,
+    "999999-000002", 30, "J0033VN", 55, 4,
+    "999999-000002", 30, "I0019NT", 89, 6,
+    "999999-000002", 30, "M0019LN", 990, 4,
+    "999999-000002", 30, "R0003MA", 340, 6
+  )
 
-    # expected dataset
-    expected <- input %>%
-      mutate(
-        CRIT1FL = case_when(
-          !is.na(AVAL) & !is.na(ISLLOQ) & AVAL >= ISLLOQ ~ "Y",
-          !is.na(AVAL) & !is.na(ISLLOQ) & AVAL < ISLLOQ ~ "N",
-          TRUE ~ as.character(NA)
-        ),
-        CRIT1 = if_else(!is.na(CRIT1FL), "Titer >= ISLLOQ", as.character(NA)),
-        CRIT1FN = if_else(CRIT1FL == "Y", 1, 0)
-      )
-
-    expect_warning(
-      derive_vars_crit(
-        dataset = input,
-        prefix = "CRIT1",
-        crit_label = "Titer >= ISLLOQ",
-        condition = !is.na(AVAL) & !is.na(ISLLOQ),
-        criterion = AVAL >= ISLLOQ
+  # expected dataset
+  expected <- input %>%
+    mutate(
+      CRIT1FL = case_when(
+        !is.na(AVAL) & !is.na(ISLLOQ) & AVAL >= ISLLOQ ~ "Y",
+        !is.na(AVAL) & !is.na(ISLLOQ) & AVAL < ISLLOQ ~ "N",
+        TRUE ~ as.character(NA)
       ),
-      class = "lifecycle_warning_deprecated"
+      CRIT1 = if_else(!is.na(CRIT1FL), "Titer >= ISLLOQ", as.character(NA)),
+      CRIT1FN = if_else(CRIT1FL == "Y", 1, 0)
     )
+
+  expect_warning(
+    derive_vars_crit(
+      dataset = input,
+      prefix = "CRIT1",
+      crit_label = "Titer >= ISLLOQ",
+      condition = !is.na(AVAL) & !is.na(ISLLOQ),
+      criterion = AVAL >= ISLLOQ
+    ),
+    class = "lifecycle_warning_deprecated"
+  )
 })
 
 
